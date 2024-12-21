3af5bafe47bd8fd39dc0155f10f4b7972ce849e7
Derek Eiri <31357987+derekeiri@users.noreply.github.com>
2024-12-20 22:24:47 -0800

    Update ileapp.rb
    Removed a "v" from the Intel and Silicon URLs.

--- Formula/ileapp.rb
+++ Formula/ileapp.rb
@@ -3,10 +3,10 @@ class Ileapp < Formula
   homepage "https://github.com/abrignoni/iLEAPP"
 
   if Hardware::CPU.intel?
-    url "https://github.com/abrignoni/iLEAPP/releases/download/v2.0.2/ileapp-v2.0.2-Mac_Intel.zip"
+    url "https://github.com/abrignoni/iLEAPP/releases/download/v2.0.2/ileapp-2.0.2-Mac_Intel.zip"
     sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   else
-    url "https://github.com/abrignoni/iLEAPP/releases/download/v2.0.2/ileapp-v2.0.2-Apple_Silicon.zip"
+    url "https://github.com/abrignoni/iLEAPP/releases/download/v2.0.2/ileapp-2.0.2-Apple_Silicon.zip"
     sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   end
 
