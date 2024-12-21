3d229e34418ee52c376258097e8df4019cd909fc
Derek Eiri <31357987+derekeiri@users.noreply.github.com>
2024-12-20 22:22:36 -0800

    Update ileapp-gui.rb
    Removed a "v" from the Intel and Silicon URLs.

--- Casks/ileapp-gui.rb
+++ Casks/ileapp-gui.rb
@@ -2,10 +2,10 @@ cask "ileapp-gui" do
   version "v2.0.2"
 
   if Hardware::CPU.intel?
-    url "https://github.com/abrignoni/iLEAPP/releases/download/v2.0.2/ileappGUI-v2.0.2-Mac_Intel.dmg"
+    url "https://github.com/abrignoni/iLEAPP/releases/download/v2.0.2/ileappGUI-2.0.2-Mac_Intel.dmg"
     sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   else
-    url "https://github.com/abrignoni/iLEAPP/releases/download/v2.0.2/ileappGUI-v2.0.2-Apple_Silicon.dmg"
+    url "https://github.com/abrignoni/iLEAPP/releases/download/v2.0.2/ileappGUI-2.0.2-Apple_Silicon.dmg"
     sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   end
 
